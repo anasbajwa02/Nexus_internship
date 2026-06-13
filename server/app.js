86e3e18d7b6513bc251862bfa5951f9e1143abd0
Anas Rehman <anas03154297874@gmail.com>
2026-06-13 12:11:42 +0500

    setup basic backend , install dependecies and connecctDb

---
+++ server/app.js
@@ -0,0 +1,42 @@
+import  "./src/config/env.js";
+import dns from "dns";
+dns.setServers(["1.1.1.1","8.8.8.8"])
+
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+//
+import connectDB from "./src/config/db.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// Connect to MongoDB
+
+
+
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Nexus API");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
---
+++ server/src/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async () => {
+    const MONGO_URI = `${process.env.MONGO_URI}/nexus`;
+   
+    try{
+        const conn = await mongoose.connect(MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.mongoose);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
